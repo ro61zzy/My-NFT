446522be7316db41e0b49928075474a5242e42ed
Rose Wachuka <67165120+ro61zzy@users.noreply.github.com>
2022-12-01 16:12:12 +0300

    compile and deploy NFT

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+//The Deployment
+// Import ethers from Hardhat package
+const { ethers } = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  /*
+A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+so nftContract here is a factory for instances of our GameItem contract.
+*/
+  const nftContract = await ethers.getContractFactory("NFTy");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // wait for the contract to deploy
+  await deployedNFTContract.deployed();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
